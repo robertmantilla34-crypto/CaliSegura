41f5a839ecb5b17e17656d9e435d6cbda54e68c6
robertmantilla34-crypto <robertmantilla34@icloud.com>
2026-06-15 08:42:29 -0500

    riesgos.js

---
+++ js/riesgos.js
@@ -0,0 +1,16 @@
+const riesgos = {
+
+    "Potrero Grande":"rojo",
+    "Mojica":"rojo",
+    "Charco Azul":"rojo",
+
+    "Ciudad Cordoba":"amarillo",
+    "El Vallado":"amarillo",
+
+    "Valle del Lili":"verde",
+    "Pance":"verde",
+
+    "Menga":"verde",
+    "Ciudadela Floralia":"amarillo"
+
+};
